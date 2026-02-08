ed67ab68528807da4d98a60756574f653b859062
Meet <meet29anand@gmail.com>
2026-02-09 02:23:12 +0530

    fixed cloudianry issue 5.0

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,7 +4,7 @@
     const express = require("express");
     const router = express.Router();
     const mongoose = require("mongoose");
-    const cloudinary = require("cloudinary");
+    const { v2: cloudinary } = require("cloudinary");
     const CloudinaryStorage = require("multer-storage-cloudinary");
     const multer = require("multer");
     const nodemailer = require('nodemailer');
@@ -31,14 +31,14 @@
     };
 
     // --- 1. CLOUDINARY CONFIG ---
-    cloudinary.v2.config({
+    cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     });
 
         const storage = new CloudinaryStorage({
-        cloudinary: cloudinary.v2,
+        cloudinary: cloudinary,
         params: {
             folder: "art-studio-gallery",
             allowed_formats: ["jpg", "png", "jpeg", "webp"],
